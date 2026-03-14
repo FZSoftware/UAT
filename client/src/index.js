319029e74b9a68186a169a01a30b26576aea9b04
FZSoftware <balajitkv1@gmail.com>
2026-03-14 18:57:55 +0530

    enable pwa

--- client/src/index.js
+++ client/src/index.js
@@ -2,5 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+// enable PWA
+serviceWorker.register();
